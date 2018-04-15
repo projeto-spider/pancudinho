7d066ca217b99d9d1dfd5784be068a879ec106ed
lubien <lubien1996@gmail.com>
2018-04-15 02:58:47 -0300

    Receive click handler on Card component

--- src/stories/MemoryGame.js
+++ src/stories/MemoryGame.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
 import { boolean, text, withKnobs } from '@storybook/addon-knobs/vue'
 import Centered from '@storybook/addon-centered'
+import { action } from '@storybook/addon-actions'
 
 import Card from '../components/memory-game/Card.vue'
 
@@ -16,6 +17,7 @@ stories
   .add('Card', (h) => ({
     render: h => h(Card, { props: {
       flip: boolean('Flip Card', false),
-      content: text('Content', 'A')
+      content: text('Content', 'A'),
+      handleClick: action('Click')
     } })
   }))
