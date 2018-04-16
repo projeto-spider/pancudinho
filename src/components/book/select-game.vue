599c96565bc6385be58b2574927201ed40b2a8c6
Tuby Neto <tuby_neto@hotmail.com>
2018-04-16 12:51:09 -0300

    Buttons changes

--- src/components/book/select-game.vue
+++ src/components/book/select-game.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <div class="book">
-      <p class="fairy-letter">Lorem ipsum dolor sit amet, est nullam discere intellegam ne, pro ne alterum facilisi, tibique deseruisse id per. Moderatius <span class="box" @click.prevent="(words[0].selected_gap=true) && (showOptions=true)">{{words[0].word_in_the_gap}}</span> reprehendunt has eu. Aperiri definitiones conclusionemque vix eu, atqui velit pertinacia no his, <span class="box" @click.prevent="(words[1].selected_gap=true) && (showOptions=true)">{{words[1].word_in_the_gap}}</span> mei eros civibus lobortis ne. Lorem feugiat <span class="box" @click.prevent="(words[2].selected_gap=true) && (showOptions=true)">{{words[2].word_in_the_gap}}</span> sanctus nam no, et equidem conclusionemque cum. Sit in soleat fastidii dissentiunt, per facete veritus ne. <div class="box five" @click="showTips=true">Tips</div> </p>
+      <p class="fairy-letter">Lorem ipsum dolor sit amet, est nullam discere intellegam ne, pro ne alterum facilisi, tibique deseruisse id per. Moderatius <span class="box" @click.prevent="(words[0].selected_gap = true) && (showOptions = true)">{{words[0].word_in_the_gap}}</span> reprehendunt has eu. Aperiri definitiones conclusionemque vix eu, atqui velit pertinacia no his, <span class="box" @click.prevent="(words[1].selected_gap = true) && (showOptions = true)">{{words[1].word_in_the_gap}}</span> mei eros civibus lobortis ne. Lorem feugiat <span class="box" @click.prevent="(words[2].selected_gap = true) && (showOptions = true)">{{words[2].word_in_the_gap}}</span> sanctus nam no, et equidem conclusionemque cum. Sit in soleat fastidii dissentiunt, per facete veritus ne. <div class="box five" @click="showTips = true">Tips</div> <div class="box five"  @click="delet()">Again</div></p>
     </div>
 
     <div v-if="showOptions">
       <div class="defocus">
-        <button  @click="showTips=true">Tips</button> <button  @click="delet()">Again</button> <button  @click="showOptions = false">X</button>
-        <div class="box one" @click="((words[0].selected_word=true) && select())"> {{word1}} </div>
-        <div class="box three" @click="((words[2].selected_word=true) && select())"> {{word3}} </div>
-        <div class="box two" @click="((words[1].selected_word=true) && select())"> {{word2}} </div>
+        <button style="margin-right:80px;" @click="showTips=true">Tips</button> <button style="margin-right:80px;" @click="showOptions = false">X</button>
+        <div class="box one" @click="((words[0].selected_word = true) && (select()) || (showOptions = false))"> {{word1}} </div>
+        <div class="box three" @click="((words[2].selected_word = true) && (select()) || (showOptions = false))"> {{word3}} </div>
+        <div class="box two" @click="((words[1].selected_word = true) && (select()) || (showOptions = false))"> {{word2}} </div>
       </div>
     </div>
     <div v-if="showTips">
       <div class=defocus>
-        <button style="margin-left:330px;" @click="showTips=false">X</button>
+        <button style="margin-left:190px;" @click = "showTips=false">X</button>
         <div class="box four fairy-letter">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est.</div>
       </div>
     </div>
@@ -24,61 +24,57 @@
 <script type="text/javascript">
 export default {
   name: 'BookselectGame',
-
   data: () => ({
-    word1: "Lorem ",
-    word2: "Praesent blandit",
-    word3: "Nullam et",
+    word1: 'Lorem',
+    word2: 'Praesent blandit',
+    word3: 'Nullam et',
     showOptions: false,
     showTips: false,
-    words:[
-        {
-        word1: "Lorem ",
-        selected_word:false,
-        selected_gap:false,
-        word_in_the_gap:"?",
-        },
-        {
-        word1: "Praesent blandit",
-        selected_word:false,
-        selected_gap:false,
-        word_in_the_gap:"?"
-        },
-        {
-        word1: "Nullam et",
-        selected_word:false,
-        selected_gap:false,
-        word_in_the_gap:"?"
-        },
-     ],
+    words: [
+      {
+        word1: 'Lorem',
+        selected_word: false,
+        selected_gap: false,
+        word_in_the_gap: '?'
+      },
+      {
+        word1: 'Praesent blandit',
+        selected_word: false,
+        selected_gap: false,
+        word_in_the_gap: '?'
+      },
+      {
+        word1: 'Nullam et',
+        selected_word: false,
+        selected_gap: false,
+        word_in_the_gap: '?'
+      }
+    ]
   }),
-
-  methods:{
-    select(){
-      for(var i=0; i<this.words.length;i++){
-        for(var j=0; j<this.words.length;j++){
-          if(this.words[i].selected_word===true && this.words[j].selected_gap===true ){
-            this.words[j].word_in_the_gap=this.words[i].word1
+  methods: {
+    select () {
+      for (var i = 0; i < this.words.length; i++) {
+        for (var j = 0; j < this.words.length; j++) {
+          if (this.words[i].selected_word === true && this.words[j].selected_gap === true) {
+            this.words[j].word_in_the_gap = this.words[i].word1
           }
         }
       }
-      for(var i=0; i<this.words.length;i++){
-        this.words[i].selected_gap=false
-        this.words[i].selected_word=false
+      for (var i = 0; i < this.words.length; i++) {
+        this.words[i].selected_gap = false
+        this.words[i].selected_word = false
       }
     },
-
-    delet(){
-      for(var i=0; i<this.words.length;i++){
-        this.words[i].selected_gap=false
-        this.words[i].selected_word=false
-        this.words[i].word_in_the_gap="?"
+    delet () {
+      for (var i = 0; i < this.words.length; i++) {
+        this.words[i].selected_gap = false
+        this.words[i].selected_word = false
+        this.words[i].word_in_the_gap = '?'
       }
-    },
+    }
   }
 }
 </script>
-
 <style type="text/css">
 .book {
   padding: 1.6em 2.3em 1.4em;
@@ -158,8 +154,10 @@ export default {
 }
 
 button{
+  text-align: center;
+  padding: 0.3em 0.3em;
   width: 90px;
-  margin-top:50px;
+  margin-top:30px;
   margin-left: 50px;
   border-radius: 10px;
   background: MediumOrchid;
@@ -167,7 +165,6 @@ button{
   font: 400 30px/1.3 'Arizonia', Helvetica, sans-serif;
   text-shadow: 4px 4px 0px rgba(0,0,0,0.2);
 }
-
 .defocus {
   position: fixed;
   z-index: 2;
@@ -189,4 +186,3 @@ button{
   text-shadow: 4px 4px 0px rgba(0,0,0,0.1);
 }
 </style>
-
