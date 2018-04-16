8aac5aa9d416bf4a76545dd93a4e6c44a5f50b11
lubien <lubien1996@gmail.com>
2018-04-16 12:57:05 -0300

    Add Board component for Memory Game

--- src/stories/MemoryGame.js
+++ src/stories/MemoryGame.js
@@ -1,11 +1,12 @@
 import Vue from 'vue' // eslint-disable-line
 
 import { storiesOf } from '@storybook/vue'
-import { boolean, text, withKnobs } from '@storybook/addon-knobs/vue'
+import { boolean, text, object, withKnobs } from '@storybook/addon-knobs/vue'
 import Centered from '@storybook/addon-centered'
 import { action } from '@storybook/addon-actions'
 
 import Card from '../components/memory-game/Card.vue'
+import Board from '../components/memory-game/Board.vue'
 
 const stories = storiesOf('Memory Game', module)
 
@@ -20,4 +21,30 @@ stories
       content: text('Content', 'A'),
       handleClick: action('Click')
     } })
+  }))
+  .add('Board', () => ({
+    template: `
+      <Board
+        :cards="cards"
+        :handle-click-one="handleClickOne"
+      ></Board>
+    `,
+
+    components: { Board },
+
+    data: () => ({
+      cards: object('Cards', Array.from(Array(26), (_, i) => ({
+        id: i + 1,
+        flip: false,
+        content: String.fromCharCode('A'.charCodeAt(0) + i)
+      })))
+    }),
+
+    methods: {
+      handleClickOne (card) {
+        card.flip = !card.flip
+        action('Clicked on Card')(card)
+        console.log('a')
+      }
+    }
   }))
