b5059471442ceb55ebb233b122db4976865311df
lubien <lubien1996@gmail.com>
2018-04-16 12:57:05 -0300

    Add Board component for Memory Game

---
+++ src/components/memory-game/Board.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="Board">
+    <Card
+      v-for="card in cards"
+      :key="card.id"
+      :flip="card.flip"
+      :content="card.content"
+      :handle-click="() => clickedOne(card)"
+    ></Card>
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue'
+
+export default {
+  name: 'Board',
+
+  components: { Card },
+
+  props: {
+    cards: {
+      type: Array,
+      default: () => []
+    },
+
+    handleClickOne: {
+      type: Function,
+      default: () => {}
+    }
+  },
+
+  methods: {
+    clickedOne (card) {
+      this.handleClickOne(card)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Board {
+  display: flex;
+  border: 1px solid #dedede;
+  padding: 15px;
+  max-width: 90vw;
+  flex-wrap: wrap;
+  justify-content: center
+}
+</style>
