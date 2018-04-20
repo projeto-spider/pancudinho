23ef11ecc9283b81fd225ad1a0033a5022dffd0a
master <henriquesponte@hotmail.com>
2018-04-19 23:27:57 -0300

    Add component of pancudinho

---
+++ src/components/memory-game/Pancudinho.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Pancudinho">
+    <img src="../../assets/pancudinho.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pancudinho'
+}
+</script>
+
+<style scoped>
+.Pancudinho img {
+  transform: scaleX(-1);
+}
+</style>
