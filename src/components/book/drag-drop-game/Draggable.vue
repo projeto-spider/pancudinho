b30782bfce16a515cc490bd5ac63e6e48cc56266
leochrisis <leonardo.chfc@gmail.com>
2018-04-25 11:48:38 -0300

    Start draggable component

---
+++ src/components/book/drag-drop-game/Draggable.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="draggable">
+      <h1>olar</h1>
+    </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'Draggable'
+}
+</script>
+
+<style>
+.draggable {
+}
+</style>
