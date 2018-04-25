b56e6e0b25be7d422c84b1e0ad90fbc2f0959648
leochrisis <leonardo.chfc@gmail.com>
2018-04-25 11:50:08 -0300

    Create book's drag and drop game story

---
+++ src/stories/BookDragDropGame.js
@@ -0,0 +1,19 @@
+import Vue from 'vue' // eslint-disable-line
+
+import { storiesOf } from '@storybook/vue'
+import { withKnobs } from '@storybook/addon-knobs/vue'
+import Centered from '@storybook/addon-centered'
+
+import Draggable from '../components/book/drag-drop-game/Draggable.vue'
+
+const stories = storiesOf('Book Drag n Drop', module)
+
+stories
+  .addDecorator(withKnobs)
+  .addDecorator(Centered)
+
+stories
+  .add('Draggable', (h) => ({
+    render: h => h(Draggable, { props: {
+    } })
+  }))
