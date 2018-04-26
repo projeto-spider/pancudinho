b53200eec42f3a6d6eb6f0e8145ff9865486dbed
leochrisis <leonardo.chfc@gmail.com>
2018-04-26 12:07:34 -0300

    Add Draggable propriety to BookDragDropGame.js

--- src/stories/BookDragDropGame.js
+++ src/stories/BookDragDropGame.js
@@ -1,7 +1,7 @@
 import Vue from 'vue' // eslint-disable-line
 
 import { storiesOf } from '@storybook/vue'
-import { withKnobs } from '@storybook/addon-knobs/vue'
+import { text, withKnobs } from '@storybook/addon-knobs/vue'
 import Centered from '@storybook/addon-centered'
 
 import Draggable from '../components/book/drag-drop-game/Draggable.vue'
@@ -15,5 +15,6 @@ stories
 stories
   .add('Draggable', (h) => ({
     render: h => h(Draggable, { props: {
+      content: text('Content', 'Lorem inpsun dolor')
     } })
   }))
