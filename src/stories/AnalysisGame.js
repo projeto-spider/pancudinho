a2c0661f11eb5d25d2812f9b0bee6a55a3815acd
Victoria <av_vitoria503@hotmail.com>
2018-04-27 16:45:10 -0300

    Add VisualNovel propriety to AnalysisGame.js

--- src/stories/AnalysisGame.js
+++ src/stories/AnalysisGame.js
@@ -5,6 +5,7 @@ import { text, withKnobs } from '@storybook/addon-knobs/vue'
 import Centered from '@storybook/addon-centered'
 
 import Tips from '../components/analysis-game/Tips.vue'
+import VisualNovel from '../components/analysis-game/VisualNovel.vue'
 
 const stories = storiesOf('Analysis Game', module)
 
@@ -16,5 +17,11 @@ stories
   .add('Tips', (h) => ({
     render: h => h(Tips, { props: {
       Tip: text('Content', 'Lorem inpsun dolor')
+    } })
+  }))
+  .add('VisualNovel', (h) => ({
+    render: h => h(VisualNovel, { props: {
+      Tip: text('Content', 'Lorem inpsun dolor')
+
     } })
   }))
