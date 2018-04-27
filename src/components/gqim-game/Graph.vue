1d7aa7ffc4abf2c1adfe179a934793ac278b9ad0
lubien <lubien1996@gmail.com>
2018-04-27 11:45:36 -0300

    Put Graph nodes and edges in component data

--- src/components/gqim-game/Graph.vue
+++ src/components/gqim-game/Graph.vue
@@ -12,7 +12,8 @@ export default {
   name: 'Graph',
 
   data: () => ({
-
+    nodes: false,
+    edges: false
   }),
 
   mounted () {
@@ -26,15 +27,16 @@ export default {
         network = null
       }
 
-      const nodes = new DataSet([
+      this.nodes = new DataSet([
         {id: 1, label: 'Node 1'},
         {id: 2, label: 'Node 2'},
         {id: 3, label: 'Node 3'},
         {id: 4, label: 'Node 4'},
-        {id: 5, label: 'Node 5'}
+        {id: 5, label: 'Node 5'},
+        {id: 6, label: 'Node 6'}
       ])
 
-      const edges = new DataSet([
+      this.edges = new DataSet([
         {from: 1, to: 3},
         {from: 1, to: 2},
         {from: 2, to: 4},
@@ -43,7 +45,7 @@ export default {
       ])
 
       const $container = this.$refs.el
-      const data = { nodes, edges }
+      const data = { nodes: this.nodes, edges: this.edges }
       const options = {
         layout: {
           hierarchical: {
