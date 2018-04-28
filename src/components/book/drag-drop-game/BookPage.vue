4c5710878a90af61d36fbeb8a6b5306954f82fe8
Leochrisis <leonardo.chfc@gmail.com>
2018-04-28 17:33:10 -0300

    Start BookPage component

---
+++ src/components/book/drag-drop-game/BookPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="book">
+    <p class="fairy-letter">Lorem ipsum dolor sit amet, id quo paulo scaevola. Eu everti feugait vel, utamur discere ne duo. Ex odio adhuc comprehensam eos, cum dicunt maiestatis ad. Est mediocrem vituperata in.</p>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'BookPage'
+}
+</script>
+
+<style type="text/css">
+.book {
+  padding: 1.6em 2.3em 1.4em;
+  margin: 4.6em 20em;
+  border-radius: 3px;
+  text-align: left;
+  background: #fff6d1 /*Alternative color: #f3eded*/
+}
+.fairy-letter{
+  font: 400 32px/1.3 'Arizonia', Helvetica, sans-serif;
+  color: #2b2b2b;
+  text-shadow: 4px 4px 0px rgba(0,0,0,0.1);
+}
+</style>
