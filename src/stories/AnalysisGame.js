8784f6a52f8783949b371fc265eb451ce429401f
Victoria <av_vitoria503@hotmail.com>
2018-04-28 18:04:20 -0300

    Modified property

--- src/stories/AnalysisGame.js
+++ src/stories/AnalysisGame.js
@@ -22,11 +22,11 @@ stories
   }))
   .add('VerificationSystem', (h) => ({
     render: h => h(VerificationSystem, { props: {
-      Tip: text('Answer Correct', 'Lorem' )
+      Tip: text('Answer Correct', 'Lorem')
     } })
   }))
   .add('VisualNovel', (h) => ({
     render: h => h(VisualNovel, { props: {
-      Tip: text('Tip:', 'Lorem ipsum')
+      Tip: text('Tip:', 'Lorem ipsum dolor')
     } })
   }))
