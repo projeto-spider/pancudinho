560f6d954ec70e41f5034175334de0a6d509aef5
Leochrisis <leonardo.chfc@gmail.com>
2018-04-28 18:47:28 -0300

    Add Tip's button component

---
+++ src/components/book/drag-drop-game/TipButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="open = true">
+      Tip
+    </button>
+    <tip
+      :showTip="open"
+      :tip="tip"
+      :closeTip="closeTip"
+    >
+    </tip>
+</div>
+</template>
+
+<script type="text/javascript">
+import Tip from './Tip.vue'
+
+export default {
+  name: 'TipButton',
+
+  components: { Tip },
+
+  data: () => ({
+    tip: 'Lorem inpsun dolor',
+    open: false
+  }),
+
+  methods: {
+    closeTip () {
+      this.open = false
+    }
+  }
+}
+</script>
+
+<style type="text/css">
+</style>
