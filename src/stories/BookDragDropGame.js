3dddbe7d00ee05e89632d2c111578aa93ea5eb49
Leochrisis <leonardo.chfc@gmail.com>
2018-04-28 17:22:29 -0300

    Add Droppable component to BookDragDropGame.js

--- src/stories/BookDragDropGame.js
+++ src/stories/BookDragDropGame.js
@@ -5,6 +5,7 @@ import { text, withKnobs } from '@storybook/addon-knobs/vue'
 import Centered from '@storybook/addon-centered'
 
 import Draggable from '../components/book/drag-drop-game/Draggable.vue'
+import Droppable from '../components/book/drag-drop-game/Droppable.vue'
 
 const stories = storiesOf('Book Drag n Drop', module)
 
@@ -17,4 +18,7 @@ stories
     render: h => h(Draggable, { props: {
       content: text('Content', 'Lorem inpsun dolor')
     } })
+  }))
+  .add('Droppable', (h) => ({
+    render: h => h(Droppable, { props: {} })
   }))
