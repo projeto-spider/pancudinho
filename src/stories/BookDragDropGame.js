493064a7d64efe2bb48a11ad5a3f228220e3cf84
Leochrisis <leonardo.chfc@gmail.com>
2018-04-28 18:24:18 -0300

    Add Tip component to BookDragDropGame.js

--- src/stories/BookDragDropGame.js
+++ src/stories/BookDragDropGame.js
@@ -1,12 +1,14 @@
 import Vue from 'vue' // eslint-disable-line
 
 import { storiesOf } from '@storybook/vue'
-import { text, withKnobs } from '@storybook/addon-knobs/vue'
+import { text, withKnobs, boolean } from '@storybook/addon-knobs/vue'
 import Centered from '@storybook/addon-centered'
+import { action } from '@storybook/addon-actions'
 
 import Draggable from '../components/book/drag-drop-game/Draggable.vue'
 import Droppable from '../components/book/drag-drop-game/Droppable.vue'
 import BookPage from '../components/book/drag-drop-game/BookPage.vue'
+import Tip from '../components/book/drag-drop-game/Tip.vue'
 
 const stories = storiesOf('Book Drag n Drop', module)
 
@@ -25,4 +27,11 @@ stories
   }))
   .add('Book Page', (h) => ({
     render: h => h(BookPage, { props: {} })
+  }))
+  .add('Tip', (h) => ({
+    render: h => h(Tip, { props: {
+      tip: text('Tip', 'Lorem ipsum dolor sit amet, an pro eruditi prodesset tincidunt.'),
+      showTip: boolean('Show tip', false),
+      closeTip: action('Closing window')
+    } })
   }))
