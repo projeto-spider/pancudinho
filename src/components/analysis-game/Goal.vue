6cb938e14de73308c2d4ca9d858b0eebd7507d8c
Leochrisis <leonardo.chfc@gmail.com>
2018-04-29 21:45:04 -0300

    Create Goal component to identify the game goal

---
+++ src/components/analysis-game/Goal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="goalbox s1">
+      {{ goal }}
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'Goal',
+
+  props: {
+    goal: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style type="text/css">
+.goalbox {
+  width: 23em;
+  margin: 0 15%;
+  background: #ccedf3;
+  padding: 5%;
+  text-align: center;
+  position: relative;
+  border-radius: 30px;
+  width: 15em;
+}
+.s1:before {
+  content: "";
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  border-left: 10px solid #ccedf3;
+  border-right: 10px solid transparent;
+  border-top: 10px solid #ccedf3;
+  border-bottom: 10px solid transparent;
+  left: 19px;
+  bottom: -19px;
+}
+</style>
