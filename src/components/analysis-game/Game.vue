3fd854d11ceec1e85d7cf36505f0fe781f49bfbb
Leochrisis <leonardo.chfc@gmail.com>
2018-04-29 21:55:01 -0300

    Start Analysis Game component

---
+++ src/components/analysis-game/Game.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Hey, I'm here
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'AnalysisGame'
+}
+</script>
+
+<style type="text/css">
+</style>
