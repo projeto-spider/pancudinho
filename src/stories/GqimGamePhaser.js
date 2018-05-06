6c8568270edbc6e6287beaa8123ba4c3d1de6569
Tuby Neto <tuby_neto@hotmail.com>
2018-05-05 23:16:10 -0300

    Addition of Zoom and Drag And Drop Systems

--- src/stories/GqimGamePhaser.js
+++ src/stories/GqimGamePhaser.js
@@ -30,3 +30,75 @@ stories
       }
     } })
   }))
+  .add('Zoom and Drag-n-Drop Systems', (h) => ({
+    render: h => h(Phaser, { props: {
+      config: {
+        physics: {
+          default: 'arcade'
+        },
+        scene: {
+          preload () {},
+          create () {
+            const camera = this.cameras.main
+            camera.setBackgroundColor('#bdbdbd')
+            const lorem = 'NAOCMD – Nota do aluno obtida em cada módulo da disciplina'
+            new GqimNode(this, 50, 400, lorem) // eslint-disable-line
+            new GqimNode(this, 50, 100, '2' + lorem) // eslint-disable-line
+            let isDraggingSomething = false
+            let paddingDrag = { x: 0, y: 0 }
+
+            this.input.on('dragstart', function dragstart (pointer, gameObject) {
+              paddingDrag.x = pointer.x - gameObject.x
+              paddingDrag.y = pointer.y - gameObject.y
+            })
+            this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+              isDraggingSomething = true
+              if (camera.zoom < 0.7) {
+                const { x, y } = camera.getWorldPoint(pointer.position.x, pointer.position.y)
+                gameObject.x = x - (paddingDrag.x * Math.min(camera.zoom, 1))
+                gameObject.y = y - (paddingDrag.y * Math.min(camera.zoom, 1))
+              } else {
+                gameObject.x = dragX
+                gameObject.y = dragY
+              }
+            })
+
+            this.input.on('dragend', function dragend (pointer, gameObject) {
+              isDraggingSomething = false
+            })
+
+            let lastPointerPosition = { x: 0, y: 0 }
+
+            this.sys.canvas.onmousewheel = function onmousewheel ({ deltaY }) {
+              console.log(arguments)
+              camera.setZoom(
+                camera.zoom - (deltaY / 2000)
+              )
+            }
+
+            let originDragPoint = null
+
+            this.input.on('pointermove', function pointermove (pointer, objects) {
+              if (isDraggingSomething) return
+              lastPointerPosition.x = pointer.x
+              lastPointerPosition.y = pointer.y
+
+              if (pointer.isDown) {
+                if (originDragPoint) {
+                  camera.setScroll(
+                    camera.scrollX + (originDragPoint.x - pointer.x),
+                    camera.scrollY + (originDragPoint.y - pointer.y)
+                  )
+                }
+
+                originDragPoint = Object.assign({}, lastPointerPosition)
+              } else {
+                originDragPoint = null
+              }
+            })
+          },
+          update () {}
+        }
+      }
+    } })
+  }))
