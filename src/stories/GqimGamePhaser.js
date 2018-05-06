f1ca925b387cc03acb6668ba4fbb86b7af1a1029
Tuby Neto <tuby_neto@hotmail.com>
2018-05-05 22:50:25 -0300

    Addition of Gqim Game Phaser and Node component

--- src/stories/GqimGamePhaser.js
+++ src/stories/GqimGamePhaser.js
@@ -5,6 +5,7 @@ import { withKnobs } from '@storybook/addon-knobs/vue'
 import Centered from '@storybook/addon-centered'
 
 import Phaser from '../components/gqim-game-phaser/Phaser.vue'
+import GqimNode from '../components/gqim-game-phaser/GqimNode'
 
 const stories = storiesOf('GQIM Game Phaser', module)
 
@@ -13,12 +14,17 @@ stories
   .addDecorator(Centered)
 
 stories
-  .add('Phaser', (h) => ({
+  .add('GqimNode', (h) => ({
     render: h => h(Phaser, { props: {
       config: {
+        physics: {
+          default: 'arcade'
+        },
         scene: {
           preload () {},
-          create () {},
+          create () {
+            new GqimNode(this, this.sys.canvas.width / 2, this.sys.canvas.height / 2, 'lalala') // eslint-disable-line
+          },
           update () {}
         }
       }
