25299c0d74c1d51736bd40f74e98806af925f0c3
lubien <lubien1996@gmail.com>
2018-05-12 17:06:17 -0300

    Setup GqimGame component for Phaser

--- src/stories/GqimGamePhaser.js
+++ src/stories/GqimGamePhaser.js
@@ -1,11 +1,12 @@
 import Vue from 'vue' // eslint-disable-line
 
 import { storiesOf } from '@storybook/vue'
-import { withKnobs } from '@storybook/addon-knobs/vue'
+import { object, withKnobs } from '@storybook/addon-knobs/vue'
 import Centered from '@storybook/addon-centered'
 import PhaserEngine from 'phaser'
 
 import Phaser from '../components/gqim-game-phaser/Phaser.vue'
+import GqimGame from '../components/gqim-game-phaser/GqimGame.vue'
 import GqimNode from '../components/gqim-game-phaser/GqimNode'
 import DropZone from '../components/gqim-game-phaser/DropZone'
 
@@ -539,5 +540,82 @@ stories
           update () {}
         }
       }
+    } })
+  }))
+  .add('Game Component', (h) => ({
+    render: h => h(GqimGame, { props: {
+      tree: object('Tree', {
+        goal: {
+          id: 'goal-1',
+          label: 'Melhorar o desempenho do aluno',
+          edges: [
+            'question-1',
+            'question-2',
+            'question-3'
+          ]
+        },
+
+        questions: [
+          {
+            toDrop: true,
+            id: 'question-1',
+            label: 'Qual a média dos alunos?',
+            edges: [
+              'indicator-ma'
+            ]
+          },
+          {
+            id: 'question-2',
+            label: 'Quais notas obtidas em cada módulo da disciplina?',
+            edges: [
+              'indicator-naocmd'
+            ]
+          },
+          {
+            id: 'question-3',
+            label: 'Qual é a frequência do aluno?',
+            edges: [
+              'indicator-fa'
+            ]
+          }
+        ],
+
+        indicators: [
+          {
+            id: 'indicator-ma',
+            label: 'MA – Média dos alunos',
+            edges: [
+              'metric-1'
+            ]
+          },
+          {
+            toDrop: true,
+            id: 'indicator-naocmd',
+            label: 'NAOCMD – Nota do aluno obtida em cada módulo da disciplina',
+            edges: [
+              'metric-1'
+            ]
+          },
+          {
+            id: 'indicator-fa',
+            label: 'FA – Frequência do aluno',
+            edges: [
+              'metric-2'
+            ]
+          }
+        ],
+
+        metrics: [
+          {
+            id: 'metric-1',
+            label: 'Nota'
+          },
+          {
+            toDrop: true,
+            id: 'metric-2',
+            label: 'Frequência'
+          }
+        ]
+      })
     } })
   }))
