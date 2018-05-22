6f3265823d139b1506723ea9b7c40abda244e941
Victoria <av_vitoria503@hotmail.com>
2018-05-21 22:56:24 -0300

    Select Game property added in storybook/config

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,6 +6,7 @@ import 'milligram/dist/milligram.css'
 function loadStories() {
   require('../src/stories/MemoryGame')
   require('../src/stories/BookDragDropGame')
+  require('../src/stories/Select-Game')
 }
 
 configure(loadStories, module);
