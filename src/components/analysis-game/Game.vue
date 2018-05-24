78e5969c9f77727398e8ec2051037a92dee7d58b
leochrisis <leonardo.chfc@gmail.com>
2018-05-24 10:12:48 -0300

    Import all needed components to Analysis Game

--- src/components/analysis-game/Game.vue
+++ src/components/analysis-game/Game.vue
@@ -5,8 +5,28 @@
 </template>
 
 <script type="text/javascript">
+import Tips from './Tips.vue'
+import VisualNovel from './VisualNovel.vue'
+import Draggable from './Draggable.vue'
+import Droppable from './Droppable'
+import Goal from './Goal.vue'
+
 export default {
-  name: 'AnalysisGame'
+  name: 'AnalysisGame',
+
+  components: {
+    Tips,
+    VisualNovel,
+    Draggable,
+    Droppable,
+    Goal
+  },
+
+  data: () => ({
+  }),
+
+  methods: {
+  }
 }
 </script>
 
