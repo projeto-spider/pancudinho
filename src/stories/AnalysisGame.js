de02029766d084bd7ec23479e2cfbbe170a03b84
leochrisis <leonardo.chfc@gmail.com>
2018-05-24 11:51:15 -0300

    Remove unused Tip property

--- src/stories/AnalysisGame.js
+++ src/stories/AnalysisGame.js
@@ -31,7 +31,6 @@ stories
   }))
   .add('VisualNovel', (h) => ({
     render: h => h(VisualNovel, { props: {
-      Tip: text('Tip:', 'Lorem ipsum dolor')
       talks: object('Talks', [
         {task: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia risus ut orci hendrerit rhoncus. Etiam imperdiet, sapien non feugiat molestie.'},
         {task: 'Usce efficitur posuere elit, varius gravid dolor feugiat eget. Vestibulum condimentum odio ac erat fermentum tincidunt.'},
