39662a1ef8c8c060aa25f0a0d1d9f76e7e3384de
Victoria <av_vitoria503@hotmail.com>
2018-05-29 00:44:46 -0300

    Selectable component update

--- src/components/book/select-fill-game/Selectable.vue
+++ src/components/book/select-fill-game/Selectable.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-    <span class="box" @click="defocus = true"></span>
-    <div v-show="defocus">
+
+<div>
+  <span class="box" @click="defocus = true"></span>
+    <div v-if="defocus">
       <div class="defocus" @click="defocus = false">
-        <div v-for="option in options" :key="option.id">
-          <div class="option"> {{option.text}} </div>
+        <div v-for="option in options" :key="option.id" >
+            <div class="option"> {{option.words}} </div>
+
         </div>
       </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script type="text/javascript">
@@ -16,30 +18,65 @@ export default {
   name: 'Selectable',
 
   props: {
+    /*
+      defocus: {
+      type: Boolean,
+      required: false
+    }
+
+      options: {
+      type: Array,
+      default: () => []
+    }
+    */
   },
 
   data: () => ({
+
     options: [
       {
-        text: 'lalala',
+        text: 'Lorem ipsum dolor sit amet, est nullam discere intellegam ne, pro ne alterum facilisi, tibique deseruisse id per. Moderatius',
+        words: 'lorem',
         id: 1
       },
 
       {
-        text: 'lelele',
+        text: 'reprehendunt has eu. Aperiri definitiones conclusionemque vix eu, atqui velit pertinacia no his,',
+        words: 'ipsum',
         id: 2
       }
     ],
+
     defocus: false
   })
 }
 </script>
 
 <style type="text/css" scoped>
+
+.book{
+  padding: 5em 3em 3em 3em;
+  margin: 1em 5em;
+  border-radius: 3px;
+  text-align: left;
+  background: #fff6d1; /*Alternative color: #f3eded*/
+  box-shadow: .2em .2em .5em #333
+}
+
 .box {
+  /*
   padding: 20px 110px;
-  background: #2cda82;
+  background: rgba(219, 201, 201, 0.133);
   border-radius: 5px;
+  */
+  background: #bdbdbd;
+  border-radius: 5px;
+  opacity: 0.5;
+  display: block;
+  padding: 20px 110px;
+  margin: 0;
+
+
 }
 
 .defocus {
@@ -60,12 +97,20 @@ export default {
   margin: 0.4em 1em;
   margin-top: 4cm;
   width: 60px;
-  margin-left: 150px;
+  margin-left: 130px;
   background: plum;
   box-shadow: 5px 5px 5px rgba(0,0,0,0.8);
   font: 400 23px/1.3 'Arizonia', Helvetica, sans-serif;
   font-weight: bold;
   text-shadow: 4px 4px 0px rgba(0,0,0,0.2);
-  color: black
+  color: black;
+  float:left;
 }
+
+.fairy-letter {
+  font: 400 20px/1.3 'Arizonia', Helvetica, sans-serif;
+  color: #2b2b2b;
+  text-shadow: 4px 4px 0px rgba(0,0,0,0.1);
+}
+
 </style>
