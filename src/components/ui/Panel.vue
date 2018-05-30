af5dd508c0946a8eee5cfb8ef2739562a248bc00
lubien <lubien1996@gmail.com>
2018-05-30 00:26:38 -0300

    Suport multiple panel colors

--- src/components/ui/Panel.vue
+++ src/components/ui/Panel.vue
@@ -1,19 +1,54 @@
 <template>
-  <div class="Panel">
+  <div class="Panel" :class="computedClass">
     <slot></slot>
   </div>
 </template>
 
 <script>
+const validColors = new Set(['blue', 'red', 'green', 'yellow', 'grey'])
+
 export default {
-  name: 'Panel'
+  name: 'Panel',
+
+  props: {
+    color: {
+      type: String,
+      default: 'grey',
+      validator (value) {
+        return validColors.has(value)
+      }
+    }
+  },
+
+  computed: {
+    computedClass () {
+      return {
+        [this.color]: true
+      }
+    }
+  }
 }
 </script>
 
 <style>
-.Panel{
+.Panel {
   border-style: solid;
   border-width: 27px;
+}
+
+.Panel.blue {
+  border-image: url('../../assets/blue_panel.png') 27 fill repeat;
+}
+.Panel.red {
+  border-image: url('../../assets/red_panel.png') 27 fill repeat;
+}
+.Panel.green {
+  border-image: url('../../assets/green_panel.png') 27 fill repeat;
+}
+.Panel.yellow {
+  border-image: url('../../assets/yellow_panel.png') 27 fill repeat;
+}
+.Panel.grey {
   border-image: url('../../assets/grey_panel.png') 27 fill repeat;
 }
 </style>
