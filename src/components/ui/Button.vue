c4aa2475cd8026628618e7e95063b1f424083a75
lubien <lubien1996@gmail.com>
2018-05-30 01:14:27 -0300

    Button support slots and different colors

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,10 +1,13 @@
 <template>
-  <button class="Button" @click="handleClick">
+  <button class="Button" :class="computedClass" @click="handleClick">
     {{ label }}
+    <slot></slot>
   </button>
 </template>
 
 <script>
+const validColors = new Set(['blue', 'red', 'green', 'grey'])
+
 export default {
   name: 'Button',
 
@@ -14,8 +17,22 @@ export default {
       default: () => ({})
     },
     label: {
+      type: String
+    },
+    color: {
       type: String,
-      required: true
+      default: 'grey',
+      validator (value) {
+        return validColors.has(value)
+      }
+    }
+  },
+
+  computed: {
+    computedClass () {
+      return {
+        [this.color]: true
+      }
     }
   }
 }
@@ -29,9 +46,25 @@ export default {
   border-style: solid;
   border-width: 7px 27px 35px;
   padding: 0 0 5px 0;
+}
+
+.Button.blue {
+  border-image: url('../../assets/blue_button00.png') 7 27 35 repeat;
+  background-color: #35baf3;
+}
+.Button.red {
+  border-image: url('../../assets/red_button00.png') 7 27 35 repeat;
+  background-color: #e86a17;
+}
+.Button.green {
+  border-image: url('../../assets/green_button00.png') 7 27 35 repeat;
+  background-color: #88e060;
+}
+.Button.grey {
   border-image: url('../../assets/grey_button00.png') 7 27 35 repeat;
   background-color: #fff;
 }
+
 .Button:active {
   margin-top: 1px;
   border-image: url('../../assets/yellow_button00.png') 7 27 35 repeat;
