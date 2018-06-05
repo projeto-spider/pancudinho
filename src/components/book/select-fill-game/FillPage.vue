4e0cbca9437c85ce38d1c19439e344ca65376fab
Tuby Neto <tuby_neto@hotmail.com>
2018-06-04 22:42:34 -0300

    Add FillPage.vue

---
+++ src/components/book/select-fill-game/FillPage.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="book">
+    <div v-for="option in options" :key="option.id">
+      <div class="fairy-letter">{{option.text}}</div>
+        <div v-if="option.right">
+          <input class='right' v-model='option.typed'>
+        </div>
+        <div v-if="option.wrong">
+          <input class='wrong' v-model='option.typed'>
+        </div>
+        <div v-if="(!option.right && !option.wrong)">
+          <input v-model='option.typed'>
+        </div>
+    </div>
+    <button @click="doAgain">Again</button><br>
+    <button @click="checkAnswers">Submit</button>
+  </div>
+</template>
+
+<script>
+import Inputble from './Inputble.vue'
+export default {
+  components: {Inputble},
+  data: () => ({
+    options: [
+      {
+       text: 'Lorem ipsum dolor sit amet, est nullam discere intellegam ne, pro ne alterum facilisi, tibique deseruisse id per. Moderatius',
+       words: 'lorem',
+       id: 1,
+       typed: '',
+       right: false,
+       wrong: false
+      },
+      {
+       text: 'reprehendunt has eu. Aperiri definitiones conclusionemque vix eu, atqui velit pertinacia no his,',
+       words: 'ipsum',
+       id: 2,
+       typed: '',
+       right: false,
+       wrong: false
+      }
+   ]
+  }),
+  methods:{
+    checkAnswers(){
+      for (var i=0;i<this.options.length;i++){
+        if (this.options[i].typed===this.options[i].words){
+          this.options[i].right=true
+          this.options[i].wrong=false
+        }else{
+          this.options[i].right=false
+          this.options[i].wrong=true
+        }
+      }
+    },
+    doAgain(){
+      for (var i=0;i<this.options.length;i++){
+        this.options[i].typed=''
+        this.options[i].right=false
+        this.options[i].wrong=false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.book{
+  padding: 5em 3em 3em 3em;
+  margin: 1em 5em;
+  border-radius: 3px;
+  text-align: left;
+  background: #fff6d1; /*Alternative color: #f3eded*/
+  box-shadow: .2em .2em .5em #333
+}
+.fairy-letter {
+  font: 400 20px/1.3 'Arizonia', Helvetica, sans-serif;
+  color: #2b2b2b;
+  text-shadow: 4px 4px 0px rgba(0,0,0,0.1);
+}
+input{
+  padding: 0.2em 0.1em;
+  margin: 0.2em 0.2em;
+  width:266px;
+  background: #ffffff;
+  border-radius: 5px;
+  text-align: center;
+  font: 40 35px/0.1 'Arizonia', Helvetica, sans-serif;
+  color: #2b2b2b;
+  text-shadow: 4px 4px 0px rgba(0,0,0,0.1);
+}
+.right{
+  background-color:#12f51963;
+}
+.wrong{
+  background-color:#f5121263;
+}
+
+</style>
