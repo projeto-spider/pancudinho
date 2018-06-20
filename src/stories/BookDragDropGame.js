c2fb39acc48e9ffac2deb115fc6123f0a811692d
Victoria <av_vitoria503@hotmail.com>
2018-06-20 00:19:32 -0300

    Standardization of BookPage: background componet rendering

--- src/stories/BookDragDropGame.js
+++ src/stories/BookDragDropGame.js
@@ -66,6 +66,49 @@ stories
       ])
     } })
   }))
+
+
+  .add('Book Page with background', (h) => ({
+    render: h => h(Background, { props: {
+    } }, [
+    h(BookPage, { props: {
+      items: object('Items', [
+        {
+          id: 1,
+          text: 'Lorem ipsum dolor sit amet,'
+        },
+        {
+          id: 2,
+          text: 'Lurest farm',
+          isAnswer: true
+        },
+        {
+          id: 3,
+          text: 'id quo paulo scaevola. Eu everti feugait vel, utamur discere ne duo.'
+        },
+        {
+          id: 4,
+          text: 'Loren impsun',
+          isAnswer: true
+        },
+        {
+          id: 5,
+          text: 'Ex odio adhuc comprehensam eos, cum dicunt maiestatis ad.'
+        },
+        {
+          id: 6,
+          text: 'Tomes net',
+          isAnswer: true
+        },
+        {
+          id: 7,
+          text: 'id quo paulo scaevola'
+        }
+      ])
+    }})
+  ])
+}))
+
   .add('Tip', (h) => ({
     render: h => h(Tip, { props: {
       tip: text('Tip', 'Lorem ipsum dolor sit amet, an pro eruditi prodesset tincidunt.'),
@@ -73,6 +116,8 @@ stories
       closeTip: action('Closing window')
     } })
   }))
+
+
 
   .add('Tip with background', (h) => ({
     render: h => h(Background, { props: {
@@ -127,6 +172,7 @@ stories
       ])
     } })
   }))
+
   .add('Game with background', (h) => ({
     render: h => h(Background, { props: {
     } }, [
