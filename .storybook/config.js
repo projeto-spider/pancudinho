b8c2c9970feee745c4825eba4bfbc38a787611fe
Leochrisis <leonardo.chfc@gmail.com>
2018-06-27 02:47:30 -0300

    Add Pages to storybook configurations

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import '../src/assets/global-fonts.css'
 
 function loadStories() {
+  require('../src/stories/Pages')
   require('../src/stories/Ui')
   require('../src/stories/MemoryGame')
   require('../src/stories/GqimGame')
