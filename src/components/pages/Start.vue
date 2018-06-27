9d219e4bfe2d864a0d8a64cf83c1c21a10401987
Leochrisis <leonardo.chfc@gmail.com>
2018-06-27 03:10:33 -0300

    Add UI's Button to provide game options

--- src/components/pages/Start.vue
+++ src/components/pages/Start.vue
@@ -1,14 +1,40 @@
 <template>
   <div>
-    Hey, I'm here!
+    <div class="margin-layout" v-for="option in options" :key="option.id">
+      <Button color="blue">{{option.text}}</Button>
+    </div>
   </div>
 </template>
 
 <script>
+import Button from '../ui/Button.vue'
+
 export default {
-  name: 'Start'
+  name: 'Start',
+
+  componets: { Button },
+
+  data: () => ({
+    options: [
+      {
+        id: 1,
+        text: 'Começar jogo'
+      },
+      {
+        id: 2,
+        text: 'Configurações'
+      },
+      {
+        id: 3,
+        text: 'Créditos'
+      }
+    ]
+  })
 }
 </script>
 
 <style scoped>
+.margin-layout {
+  margin: 20px
+}
 </style>
