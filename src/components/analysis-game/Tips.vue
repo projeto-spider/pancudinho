609298b4e5485cb89a5ce8885bae3c5552789d62
Leochrisis <leonardo.chfc@gmail.com>
2018-08-29 01:49:57 -0300

    Remove unused CSS on Tip.vue from Analysis Game

--- src/components/analysis-game/Tips.vue
+++ src/components/analysis-game/Tips.vue
@@ -23,35 +23,4 @@ export default {
 </script>
 
 <style scoped>
-.defocus {
-  position: fixed;
-  z-index: 2;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #e7e7e7;
-  display: table;
-  transition: opacity .3s ease;
-}
-button{
-  border-radius: 15px;
-  background: plum;
-  padding: 0.1px 16px;
-  box-shadow: 5px 5px 5px rgba(0,0,0,0.8);
-  font: 400 30px/1.3 'Arizonia', Helvetica, sans-serif;
-  text-shadow: 4px 4px 0px rgba(0,0,0,0.2);
-  text-align: center;
-  border: none;
-}
-.box.tip{
-  background: #bdbdbd;
-  z-index: 101;
-  margin: 18% 24%;
-  width: 50%;
-  height: 30%;
-  border-radius: 30px;
-  padding: 5%;
-}
-
 </style>
