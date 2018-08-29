80d502d13212ec46b36e666354f1d40a1df65183
Leochrisis <leonardo.chfc@gmail.com>
2018-08-29 01:23:27 -0300

    Add UI's Button to Book Drag n Drop

--- src/components/book/drag-drop-game/TipButton.vue
+++ src/components/book/drag-drop-game/TipButton.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <button type="button" @click="open = true">
-      Tip
-    </button>
+    <Button :label="'Tip'" :handle-click="() => open = true"></Button>
     <Tip :open="open" :handle-close="() => open = false">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris commodo libero et facilisis porta. Morbi molestie est eu augue euismod faucibus. Nullam id laoreet neque, eget placerat urna. Phasellus et odio facilisis purus aliquam pellentesque. Nulla facilisi. Nulla facilisi. Mauris sit amet nisi nec velit feugiat mattis non ut nunc.
     </Tip>
@@ -10,12 +8,13 @@
 </template>
 
 <script type="text/javascript">
+import Button from '../../ui/Button.vue'
 import Tip from '../../ui/Tip.vue'
 
 export default {
   name: 'TipButton',
 
-  components: { Tip },
+  components: { Tip, Button },
 
   data: () => ({
     open: false
