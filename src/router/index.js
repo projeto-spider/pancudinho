f23a1c5a8f4390fbbe01d2d215250b2a6ca87f5e
lubien <lubien1996@gmail.com>
2018-11-14 01:14:50 -0300

    Remove wrongly used component on router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import BookSelectGame from '@/components/book/select-game'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path: '/book/Select-game/select-game',
-      name: 'BookSelectGame',
-      component: BookSelectGame
     }
   ]
 })
