5ba57889947f1e8d99133f54a2361aa359aa0316
Tuby Neto <tuby_neto@hotmail.com>
2018-11-25 23:39:17 -0300

    Show text letter by letter on Visual Novel

--- src/components/visual-novel/VisualNovel.vue
+++ src/components/visual-novel/VisualNovel.vue
@@ -1,6 +1,6 @@
 <template>
   <Panel>
-    <div v-for="(scene, index) in scenes" :key="scene.id" v-show="scene.showScene">
+    <div v-for="(scene, index) in scenes" :key="scene.id" v-show="index === counter">
       <div>
         <img :src="images[index]">
       </div>
@@ -9,7 +9,7 @@
         class='text'
         :class="{ 'text-fix': [0, 5, 8, 9, 10, 11].includes(index) }"
       >
-        {{ scene.text }}
+        {{ showText }}
       </div>
     </div>
     <div v-if='!showButton' class='btn' @click="nextText()">>></div>
@@ -27,88 +27,73 @@ export default {
       {
         text: 'Olá, eu sou Pançudinho.',
         image: 'Quadro1.jpg',
-        showText: '',
-        showScene: true
+        showText: ''
       },
       {
         text: 'Eu sei que a minha estética está ótima do jeito que está.',
-        image: 'Quadro2.jpg',
-        showScene: false
+        image: 'Quadro2.jpg'
       },
       {
         text: 'Mas notei que tenho me alimentado de um jeito bem deprimente.',
-        image: 'Quadro3.jpg',
-        showScene: false
+        image: 'Quadro3.jpg'
       },
       {
         text: 'Então decidi que a partir de hoje minha vida vai mudar.',
-        image: 'Quadro4.jpg',
-        showScene: false
+        image: 'Quadro4.jpg'
       },
       {
         text: 'Com alimentação saudável e exercícios físicos. Mas preciso de ajuda para isso.',
-        image: 'Quadro5.jpg',
-        showScene: false
+        image: 'Quadro5.jpg'
       },
       {
         text: 'Venha comigo se você quiser viver. Eu sou O TREINADOR.',
-        image: 'Quadro6.jpg',
-        showScene: false
+        image: 'Quadro6.jpg'
       },
       {
         text: 'Mas não é um caminho tão fácil como você pensa. Existem conceitos obscuros que você não conhece...',
-        image: 'Quadro7.jpg',
-        showScene: false
+        image: 'Quadro7.jpg'
       },
       {
         text: 'Mas como eu sou muito bondoso, eu posso ensinar para você.',
-        image: 'Quadro8.jpg',
-        showScene: false
+        image: 'Quadro8.jpg'
       },
       {
         text: 'Primeiro, temos que aprender a trabalhar com medidas, pois elas são essenciais para o nosso progresso.',
-        image: 'Quadro9.jpg',
-        showScene: false
+        image: 'Quadro9.jpg'
       },
       {
         text: 'Então, te apresento a medição. Com ela, nós vamos coletar, armazenar e relatar dados para que...',
-        image: 'Quadro10.jpg',
-        showScene: false
+        image: 'Quadro10.jpg'
       },
       {
         text: 'P: Balela isso! \n T: Como é?',
-        image: 'Quadro11.jpg',
-        showScene: false
+        image: 'Quadro11.jpg'
       },
       {
         text: 'P: Sei que você só ta fazendo isso pra levar meu dinheiro!',
-        image: 'Quadro12.jpg',
-        showScene: false
+        image: 'Quadro12.jpg'
       },
       {
         text: 'Mas quando falo que vou te ajudar, eu não tô mentindo.',
-        image: 'Quadro13.jpg',
-        showScene: false
+        image: 'Quadro13.jpg'
       },
       {
         text: 'Com a medição, você pode tomar decisões mais precisas e evidenciar melhor os pontos que quer melhorar.',
-        image: 'Quadro14.jpg',
-        showScene: false
+        image: 'Quadro14.jpg'
       },
       {
         text: 'P: Ok, ok...vamos ver o que você pode fazer \n T: Boa, garoto!',
-        image: 'Quadro15.jpg',
-        showScene: false
+        image: 'Quadro15.jpg'
       },
       {
         text: 'Agora, VAMOS COMEÇAR ESSA JORNADA!',
-        image: 'Quadro16.jpg',
-        showScene: false
+        image: 'Quadro16.jpg'
       }
     ],
-    showButton: true,
-    counter: 0,
-    counter2: 0,
+    counter: -1,
+    showText: '',
+    showTextDigitCount: 0,
+    showTextInterval: null,
     images: []
   }),
 
@@ -116,46 +101,40 @@ export default {
     this.images = this.scenes.map(scene => {
       return require(`../../assets/${scene.image}`)
     })
+
+    this.nextText()
+  },
+
+  computed: {
+    currentScene () {
+      return this.scenes[this.counter]
+    }
   },
 
   methods: {
     nextText () {
-      if (this.changeScene) {
-        this.counter++
-        if (this.counter === this.scenes.length) {
-          this.counter = 0
-          for (let i = 0; i < this.scenes.length; i++) {
-            this.scenes[i].showText = ''
-          }
-        }
-        for (let i = 0; i < this.scenes.length; i++) {
-          if (i === this.counter) this.scenes[i].showScene = true
-          else this.scenes[i].showScene = false
-        }
-        this.changeScene = false
-        this.letters()
+      this.counter++
+
+      if (this.showTextInterval) {
+        clearInterval(this.showTextInterval)
       }
-    },
-    letters () {
-      this.showButton = false
-      for (let i = 0; i < this.scenes.length; i++) {
-        if (this.scenes[i].showScene) {
-          let text = this.scenes[i].text
-          let showText = this.scenes[i].showText
-          let counter = this.counter2
-          let vm = this
-          let interval = setInterval(function () {
-            showText = showText + text[counter]
-            vm.scenes[vm.counter].showText = showText
-            counter++
-            if (counter === text.length) {
-              clearInterval(interval)
-              vm.changeScene = true
-            }
-          }, 50)
-        }
-        this.counter2 = 0
+
+      if (this.counter === this.scenes.length) {
+        this.state.closeGame()
       }
+
+      this.showText = ''
+      this.showTextDigitCount = 0
+
+      this.showTextInterval = setInterval(() => {
+        const nextChar = this.currentScene.text[this.showTextDigitCount++]
+
+        if (!nextChar) {
+          return clearInterval(this.showTextInterval)
+        }
+
+        this.showText += nextChar
+      }, 50)
     }
   }
 }
