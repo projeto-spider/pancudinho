6195c9f552b0613ff265bb7f626fc031870211f1
Tuby Neto <tuby_neto@hotmail.com>
2019-01-24 01:12:42 -0300

    Addition of Background in Height Game and Tutorial

--- src/components/height-game/TutorialPart4.vue
+++ src/components/height-game/TutorialPart4.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="aligner">
-    <Panel class="ScenePanel">
-      <Tutorial class='tutorial'></Tutorial>
+
       <Scene v-if="config" :config="config" ref="scene">
       </Scene>
+      <div>
       <Button color="blue" class="submit-button" @click.native="submit">
         Submit
       </Button>
-    </Panel>
+      <Tutorial :next-tutorial="() => nextTutorial()" class="tutorial"></Tutorial>
+    </div>
   </div>
 </template>
 
@@ -29,6 +30,12 @@ export default {
     Tutorial
   },
 
+  props: {
+    changeScene: {
+      type: Function
+    }
+  },
+
   data () {
     let barraEstadiometro, pancudinho, text
     let flag = 0
@@ -36,12 +43,10 @@ export default {
     function resize (width, height) {
       if (width === undefined) { width = this.sys.game.config.width }
       if (height === undefined) { height = this.sys.game.config.height }
-
       if (this.cameras) {
         this.cameras.resize(width, height)
       }
     }
-
     return {
       config: {
         physics: {
@@ -49,26 +54,24 @@ export default {
         },
         scene: {
           preload () {
+            this.load.image('consultorio', require('../../assets/Consultorio.png'))
             this.load.image('barra-estadiometro', require('../../assets/barra-estadiometro.png'))
             this.load.image('estadiometro', require('../../assets/estadiometro.png'))
-            this.load.image('pancudinho', require('../../assets/pancudinho.png'))
+            this.load.image('pancudinho', require('../../assets/pancudinho_medidas.png'))
           },
-
           create () {
+            this.add.image(500, 190, 'consultorio')
             this.events.on('resize', resize, this)
-
             const camera = this.cameras.main
             camera.setBackgroundColor('#bdbdbd')
-
-            this.physics.add.image(400, 230, 'estadiometro')
-            barraEstadiometro = this.physics.add.image(360, 70, 'barra-estadiometro')
-            pancudinho = this.physics.add.image(400, 420, 'pancudinho')
+            this.physics.add.image(600, 320, 'estadiometro')
+            barraEstadiometro = this.physics.add.image(600, 100, 'barra-estadiometro')
+            pancudinho = this.physics.add.image(600, 470, 'pancudinho')
             pancudinho.setImmovable()
             barraEstadiometro.setCollideWorldBounds(true)
             barraEstadiometro.setInteractive()
             pancudinho.setInteractive()
-
-            text = this.add.text(450, 100, '', { fontSize: '100px', fill: '#000' })
+            text = this.add.text(450, 150, '', { fontSize: '100px', fill: '#000' })
 
             setTimeout(function() {
               barraEstadiometro.setVelocity(0, 400)
@@ -77,8 +80,41 @@ export default {
             setTimeout(function() {
               barraEstadiometro.setVelocity(0, 0)
               showCounter = 'BOM!'
-            }, 1500)
+            }, 1460)
 
+
+            /*setTimeout(function () {
+              showCounter = 3
+            }, 1000)
+            setTimeout(function () {
+              showCounter = 2
+            }, 2000)
+            setTimeout(function () {
+              showCounter = 1
+            }, 3000)
+            setTimeout(function () {
+              showCounter = 'Go!!!'
+            }, 4000)
+            setTimeout(function () {
+              showCounter = ''
+              barraEstadiometro.setVelocity(0, Math.floor((Math.random() * 700) + 200))
+              flag = 1
+            }, 4500)
+            this.input.on('pointerdown', function () {
+              if (barraEstadiometro.y < 270 && flag === 1) {
+                this.add.text(200, 200, 'BOA TENTATIVA!', { fontSize: '60px', fill: '#000' })
+              } else if (barraEstadiometro.y >= 270 && barraEstadiometro.y < 300) {
+                this.add.text(200, 200, 'BOM!', { fontSize: '60px', fill: '#000' })
+              } else if (barraEstadiometro.y >= 300 && barraEstadiometro.y <= 320) {
+                this.add.text(200, 200, 'EXCELENTE!', { fontSize: '60px', fill: '#000' })
+              }
+              barraEstadiometro.setVelocity(0, 0)
+            }, this)
+            this.physics.add.overlap(barraEstadiometro, pancudinho, function () {
+              this.scene.pause()
+              this.add.text(200, 200, 'TENTE NOVAMENTE!', { fontSize: '60px', fill: '#000' })
+            }, null, this)
+            console.log(barraEstadiometro)*/
           },
           update () {
             text.setText(showCounter)
@@ -88,13 +124,9 @@ export default {
       }
     }
   },
-
-
-
   methods: {
     submit () {
       const { scene } = this.$refs
-
       scene.game.events.emit('submit')
     }
   }
@@ -109,16 +141,13 @@ export default {
   justify-content: center;
   align-items: center;
 }
-
 .ScenePanel {
   width: 90%;
   height: 90%;
 }
-
 .submit-button {
   float: right
 }
-
 .defocus {
   position: fixed;
   z-index: 2;
@@ -130,11 +159,4 @@ export default {
   display: table;
   transition: opacity .3s ease;
 }
-
-.tutorial {
-  position: fixed;
-  top: -480px;
-  left: 20px;
-}
-
 </style>
